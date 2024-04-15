79192001fd33cae9297e03d11339fea93233ae06
Святослав Поляков <viewtifulmadguy@gmail.com>
2024-04-15 22:35:27 +0300

    Create Book model

--- books/models.py
+++ books/models.py
@@ -1,3 +1,15 @@
 from django.db import models
 
-# Create your models here.
+
+class Book(models.Model):
+    title = models.CharField(max_length=200, verbose_name='Название')
+    author = models.CharField(max_length=200, verbose_name='Автор')
+    price = models.PositiveIntegerField(default=0, verbose_name='Цена')
+    read = models.BooleanField(default=False, verbose_name='Прочтено')
+
+    class Meta:
+        verbose_name = 'Книга'
+        verbose_name_plural = 'Книги'
+
+    def __str__(self):
+        return self.title
