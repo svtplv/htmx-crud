c365bbf61e7219d9755f193024272859d19849c4
Святослав Поляков <viewtifulmadguy@gmail.com>
2024-04-15 22:46:41 +0300

    Add templates

--- books/admin.py
+++ books/admin.py
@@ -2,8 +2,6 @@ from django.contrib import admin
 from .models import Book
 
 
+@admin.register(Book)
 class BookAdmin(admin.ModelAdmin):
     list_display = ('title', 'author', 'price', 'read')
-
-
-admin.site.register(Book, BookAdmin)
