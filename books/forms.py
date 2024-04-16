383f32755e04a5d9e34c1c2e58cae0f0b8a6d449
Святослав Поляков <viewtifulmadguy@gmail.com>
2024-04-16 14:39:28 +0300

    Add book form

---
+++ books/forms.py
@@ -0,0 +1,21 @@
+from django.forms import ModelForm, CharField, TextInput
+from .models import Book
+
+
+class BookCreateForm(ModelForm):
+    title = CharField(
+        required=False,
+        widget=TextInput(attrs={'class': 'clrtxt', 'placeholder': 'Название'})
+    )
+    author = CharField(
+        required=False,
+        widget=TextInput(attrs={'class': 'clrtxt', 'placeholder': 'Автор'})
+    )
+    price = CharField(
+        required=False,
+        widget=TextInput(attrs={'class': 'clrtxt', 'placeholder': 'Цена'})
+    )
+
+    class Meta:
+        model = Book
+        fields = ['title', 'author', 'price']
--- books/views.py
+++ books/views.py
@@ -1,3 +1,21 @@
-from django.shortcuts import render
+from django.http.response import HttpResponse
+from django.shortcuts import render, get_object_or_404
+from django.views.decorators.http import require_http_methods
 
-# Create your views here.
+from .models import Book
+from .forms import BookCreateForm
+
+
+@require_http_methods(['GET'])
+def book_list(request):
+    book_list = Book.objects.all()
+    form = BookCreateForm(auto_id=False)
+    return render(request, 'base.html', {'book_list': book_list, 'form': form})
+
+
+@require_http_methods(['POST'])
+def create_book(request):
+    form = BookCreateForm(request.POST)
+    if form.is_valid:
+        book = form.save()
+    return render(request, 'partial_book_detail.html', {'book': book})
